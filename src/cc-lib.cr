bc5f17ccf7c750c8edcfa1c011f443c0bb9cc743
Crystal Danger <cdneal091@gmail.com>
2019-11-12 08:08:17 -0700

    expanded dummy board generation

--- src/cc-lib.cr
+++ src/cc-lib.cr
@@ -1,9 +1,10 @@
 require "./cc-lib/*"
+require "http/web_socket"
 require "json"
 
 # TODO: Write documentation for `CC::Library`
 module CC
-  VERSION = "0.2.1"
+  VERSION = "0.2.2"
   @@boards = [] of Board
   @@subscriptions = {} of String => Array(HTTP::WebSocket)
   @@sockets = [] of HTTP::WebSocket
@@ -25,25 +26,6 @@ module CC
     @@boards << Board.from_json(board)
   end
 
-  def self.initialize_dummy_boards
-    initialize_board_from_json(%({"id":"fit", "name":"Fitness", "threads":[{"id":34003}]}))
-    initialize_board_from_json(%({
-      "id":"ck",
-      "name":"Cooking",
-      "flags": ["sfw"],
-      "threads":[{
-        "id":43003,
-        "posts":[{
-          "id":892345720934,
-          "text":"sux butts"
-          },{
-            "id":892345720937,
-            "text":"no u"
-            }]
-            }]
-            }))
-  end
-
   def self.board_by_id(board_id)
     board = @@boards.find { |board| board.id == board_id }
     board.not_nil!
@@ -54,7 +36,3 @@ end
 
 
 # CC.initialize_dummy_boards
-# fit = CC.board_by_id "fit"
-# puts fit.id
-#
-# fit.add_thread({id: 20789.to_u32}, {id: 82709208954.to_u64, text: "yeet"})
---
+++ src/cc-lib/dummy_boards.cr
@@ -0,0 +1,122 @@
+require "json"
+
+module CC
+  def self.initialize_dummy_boards
+    initialize_board_from_json(%(
+    {
+    	"id": "fit",
+    	"name": "Fitness",
+    	"flags": [
+    		"sfw"
+    	],
+    	"threads": [{
+    		"id": 43043,
+    		"posts": [{
+    				"id": 892345720934,
+    				"text": "sux fit` butts"
+    			},
+    			{
+    				"id": 892345720937,
+    				"text": "fortnite is for casuals"
+    			}
+    		]
+    	}, {
+    		"id": 43043,
+    		"posts": [{
+    				"id": 892345735934,
+    				"text": "fit butts"
+    			},
+    			{
+    				"id": 892245720937,
+    				"text": "rocknite best"
+    			}
+    		]
+    	}]
+    }
+    ))
+    initialize_board_from_json(%(
+    {
+    	"id": "ck",
+    	"name": "Cooking",
+    	"flags": [
+    		"sfw"
+    	],
+    	"threads": [{
+    		"id": 43003,
+    		"posts": [{
+    				"id": 892345720934,
+    				"text": "sux butts"
+    			},
+    			{
+    				"id": 892345720937,
+    				"text": "no u"
+    			}]
+    	}]
+    }
+    ))
+    initialize_board_from_json(%(
+    {
+    	"id": "v",
+    	"name": "Video Games",
+    	"flags": [
+    		"sfw"
+    	],
+    	"threads": [{
+    		"id": 43043,
+    		"posts": [{
+    				"id": 892345720934,
+    				"text": "sux vidya butts"
+    			},
+    			{
+    				"id": 892345720937,
+    				"text": "fortnite is for casuals"
+    			}
+    		]
+    	}, {
+    		"id": 43043,
+    		"posts": [{
+    				"id": 892345735934,
+    				"text": "vidya butts"
+    			},
+    			{
+    				"id": 892245720937,
+    				"text": "fortnite is the best"
+    			}
+    		]
+    	}]
+    }
+    ))
+    initialize_board_from_json(%(
+    {
+    	"id": "po",
+    	"name": "Papercraft and Origami",
+    	"flags": [
+    		"sfw"
+    	],
+    	"threads": [{
+    		"id": 43043,
+    		"posts": [{
+    				"id": 892345720934,
+    				"text": "sux origami butts"
+    			},
+    			{
+    				"id": 892345720937,
+    				"text": "fortnite is for casuals"
+    			}
+    		]
+    	}, {
+    		"id": 43043,
+    		"posts": [{
+    				"id": 892345735934,
+    				"text": "origami butts"
+    			},
+    			{
+    				"id": 892245720937,
+    				"text": "origami is the best"
+    			}
+    		]
+    	}]
+    }
+    ))
+  end
+end
