7226e602562431e6a15923d1441ac8302ef93d9e
Crystal Danger <cdneal091@gmail.com>
2019-12-16 20:10:14 -0700

    cleanup

--- src/cc-lib/post.cr
+++ src/cc-lib/post.cr
@@ -2,20 +2,10 @@ module CC
   # Belongs to a single `Thread` and `Board`.
   class Post
     include JSON::Serializable
-
-    @[JSON::Field(key: "id")]
     property id : UInt64
-
-    @[JSON::Field(key: "badges")]
     property badges = [] of String
-
-    @[JSON::Field(key: "flags")]
     property flags = [] of String
-
-    @[JSON::Field(key: "text")]
     property text : String = ""
-
-    @[JSON::Field(key: "media_link")]
     property media_link : String = ""
 
     def initialize(@id : UInt64, @badges = [] of String, @flags = [] of String, @text = "", @media_link = "")
